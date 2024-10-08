ba133f9616ee31ec97c8093e25e7009b963b2911
Solype <ambroise.jacquemet-ramirez-vega@epitech.eu>
2024-10-08 20:23:04 +0200

    [ADD] types

---
+++ player_manager/frontend/src/types/types.ts
@@ -0,0 +1,68 @@
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+export interface Infos {
+    age: number;
+    gender: string;
+    race: string;
+    name: string;
+}
+
+export interface Monitor {
+    [key: string]: number[];
+}
+
+export interface Other {
+    language: string[];
+    inventory: string[];
+    specialAbilities: string[];
+    spells: string[];
+    experience: number;
+    mana: number;
+    money: number;
+}
+
+export interface Priority {
+    [key: string]: string;
+}
+
+export interface Religion {
+    gods: { [key: string]: number };
+}
+
+export interface Roles {
+    main: string[];
+    secondary: string[];
+}
+
+export interface Skills {
+    [key: string]: [number, number];
+}
+
+
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+
+export interface HealthData {
+    name: string;
+    file: string;
+    "physical health": [number, number];
+    "mental health": [number, number];
+    "endurance health": [number, number];
+    "pathological health": [number, number];
+    mana: [number, number];
+}
+
+export interface Attributes {
+    [key: string]: number;
+}
+
+export interface PlayerData {
+    attributes: Attributes;
+    [key: string]: any;
+}
