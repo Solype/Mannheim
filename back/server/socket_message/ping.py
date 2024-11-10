404a005bdbc72457f5151a262a748e620fbd1cf3
Solype <ambroise.jacquemet-ramirez-vega@epitech.eu>
2024-11-10 15:22:52 +0100

    [ADD] basic chat

--- back/server/socket_message/ping.py
+++ back/server/socket_message/ping.py
@@ -1,27 +1,25 @@
 from server.server import sio
 
-@sio.on('ping')
-async def ping(sid):
-    print("PING", flush=True)
+@sio.on('message')
+async def message(sid, data):
+    print("<< message", flush=True)
     print("-- sid:", sid, flush=True)
-    await sio.emit('pong', to=sid)
-
-@sio.on('gping')
-async def gping(sid):
-    print("GPING", flush=True)
-    print("-- sid:", sid, flush=True)
-    await sio.emit('pong')
-
-@sio.on('rping')
-async def rping(sid):
-    """Répond à un ping avec un pong uniquement pour les utilisateurs dans la même room que le sid"""
-    print("RPING", flush=True)
-    print("-- sid:", sid, flush=True)
-    
+    print("-- data:", data, flush=True)
     # Trouver la room du client (sid)
     rooms = sio.rooms(sid)
     for room in rooms:
         if room != sid:  # Éviter d'envoyer le pong à l'utilisateur lui-même, car il est déjà dans cette room
-            await sio.emit('pong', to=room)
-            print(f"Sent 'pong' to room: {room}")
+            await sio.emit('message', data=f"<from {sid}> {data}", to=room)
+            print("-- room:", room, flush=True)
 
+
+@sio.on("join")
+async def join(sid, data):
+    print("<< join", flush=True)
+    print("-- sid:", sid, flush=True)
+    print("-- data:", data, flush=True)
+    for room in sio.rooms(sid):
+        if room != sid:
+            sio.leave_room(sid, room)
+    message(sid, "Connected to room: " + data)
+    sio.enter_room(sid, data)
--- front/src/components/PoNg.tsx
+++ front/src/components/PoNg.tsx
@@ -4,8 +4,11 @@ import { io } from 'socket.io-client';
 const SOCKET_SERVER_URL = 'http://localhost:8080'; // Remplacez par l'URL de votre serveur
 
 const SocketComponent = () => {
-    const [text, setText] = useState('');
-    const [socket, setSocket] = useState<any>(null);
+    const [ text, setText ] = useState('');
+    const [ currentRoom, setCurrentRoom ] = useState('');
+    const [ inputRoom, setInputRoom ] = useState('');
+    const [ socket, setSocket ] = useState<any>(null);
+    const [ messages, setMessages ] = useState<string[]>([]);
 
     // Créer une seule instance de socket lors du montage du composant
     useEffect(() => {
@@ -19,63 +22,57 @@ const SocketComponent = () => {
             console.log('Connected to server with ID:', socketInstance.id);
         });
 
-        socketInstance.onAny((event, data) => {
-            console.log(`Événement reçu: ${event}`, data);  // Affiche l'événement et les données reçues
+        socketInstance.on("message", (data) => {
+            setMessages((prevMessages) => [...prevMessages, data]);
         });
 
-        // Cleanup lors du démontage du composant
         return () => {
             socketInstance.disconnect();
         };
-    }, []); // Le tableau vide signifie que cet effet ne sera exécuté qu'une seule fois lors du montage du composant
+    }, []);
 
-    const message = () => {
-        if (socket) {
-            socket.emit('ping');
-            console.log('Sent ping to server');
-        }
-    };
-
-    const global_message = () => {
-        if (socket) {
-            socket.emit('gping');
-            console.log('Sent global ping to server');
-        }
-    };
-
-    const room_message = () => {
-        if (socket) {
-            socket.emit('rping');
-            console.log('Sent room ping to server');
-        }
-    };
 
     const join_room = () => {
         if (socket) {
-            socket.emit('join', text);
-            console.log('Joined room:', text);
+            socket.emit('join', inputRoom);
+            setCurrentRoom(inputRoom);
+            setMessages([]);
+        }
+    };
+
+    const send_message = () => {
+        if (socket) {
+            socket.emit('message', text);
         }
     };
 
     return (
         <div>
             <h1>Socket.IO React Example</h1>
-            <div className='flex gap-2 justify-center'>
-                <button className='border' onClick={message}>Send private</button>
-                <button className='border' onClick={global_message}>Send global</button>
-                <button className='border' onClick={room_message}>Send room</button>
-            </div>
             <br />
             <div className='flex gap-2 justify-center'>
                 <input
-                    className='border-2 border-black'
+                    className= { inputRoom !== currentRoom ? 'border-2 border-red-500' : 'border-2 border-black'}
                     type="text"
-                    value={text}
-                    onChange={(e) => setText(e.target.value)} 
+                    value={inputRoom}
+                    onChange={(e) => setInputRoom(e.target.value)} 
                 />
                 <button onClick={join_room}>Join Room</button>
             </div>
             <br />
+            <div className='flex gap-2 justify-center'>
+                <input
+                    className="border-2 border-black"
+                    type="text"
+                    value={text}
+                    onChange={(e) => setText(e.target.value)} 
+                />
+                <button onClick={send_message}>send message</button>
+                <br />
+            </div>
+            <div>
+                <p>messages : </p>{messages.map((message) => <p>{message}</p>)}
+            </div>
         </div>
     );
 };
