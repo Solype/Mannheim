2ebdcb22fac509eca569d72f2e5109b454a1bf9c
Solype <ambroise.jacquemet-ramirez-vega@epitech.eu>
2024-12-09 12:05:35 +0100

    [FIX] crash

--- back/server/routes/user_requests.py
+++ back/server/routes/user_requests.py
@@ -3,7 +3,7 @@ from server.routes.server_datatype.chara_type import *
 from server.mysql_db import getone_db, modify_db
 from server.access_manager import access_manager
 
-from server.routes.server_datatype.user_request_type import UserFriendRequest, UserCharacterSessionRequest, UserSessionRequest
+from server.routes.server_datatype.user_request_type import UserCharacterSessionRequest, UserSessionRequest
 
 @app.get("/api/my/requests/sessions", tags=["Requests"])
 async def get_sessions_requests(credentials: HTTPAuthorizationCredentials = Depends(security)) -> list[UserSessionRequest]:
