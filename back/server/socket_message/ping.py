e595fbab50ff1b3b8993d0d7720b264c2f91bcf6
Solype <ambroise.jacquemet-ramirez-vega@epitech.eu>
2024-12-11 23:07:47 +0100

    [ADD] socket actions

--- back/server/socket_message/ping.py
+++ back/server/socket_message/ping.py
@@ -1,4 +1,7 @@
 from server.server import sio
+from server.access_manager import access_manager
+from pydantic import BaseModel
+from typing import Literal, Optional
 
 @sio.on('message')
 async def message(sid, data):
@@ -14,7 +17,7 @@ async def message(sid, data):
 
 
 @sio.on("join")
-async def join(sid, data):
+async def join(sid, data : str):
     print("<< join", flush=True)
     print("-- sid:", sid, flush=True)
     print("-- data:", data, flush=True)
@@ -23,3 +26,57 @@ async def join(sid, data):
             sio.leave_room(sid, room)
     message(sid, "Connected to room: " + data)
     sio.enter_room(sid, data)
+
+
+class JoinSession(BaseModel):
+    name:   str
+    token:  str
+
+
+@sio.on("connect")
+async def connect(sid, data: JoinSession):
+    print("<< connect", flush=True)
+    print("-- sid:", sid, flush=True)
+    print("-- data:", data, flush=True)
+    if access_manager.isTokenValid(data.token):
+        sio.enter_room(sid, data.name)
+        for room in sio.rooms(sid):
+            sio.emit("join", data.name, room=room)
+    else:
+        sio.disconnect(sid)
+
+
+@sio.on("disconnect")
+async def disconnect(sid):
+    print("<< disconnect", flush=True)
+    print("-- sid:", sid, flush=True)
+    for room in sio.rooms(sid):
+        sio.leave_room(sid, room)
+
+class Damage(BaseModel):
+    damage:     int
+    receiver:   int
+    dealer:     int
+    monitor:    Literal["Physical", "Mental", "Pathological", "Endurance", "Mana"]
+    method:     Optional[str]
+
+@sio.on("damage")
+async def damage(sid, data: Damage):
+    print("<< damage", flush=True)
+    print("-- sid:", sid, flush=True)
+    print("-- data:", data, flush=True)
+    for room in sio.rooms(sid):
+        sio.emit("damage", data, room=room)
+
+class Heal(BaseModel):
+    heal:       int
+    dealer:     int
+    receiver:   int
+
+@sio.on("heal")
+async def heal(sid, data: Heal):
+    print("<< heal", flush=True)
+    print("-- sid:", sid, flush=True)
+    print("-- data:", data, flush=True)
+    for room in sio.rooms(sid):
+        sio.emit("heal", data, room=room)
