3b8d4f8af4d6beec6d48fb57bccfe106a38c3913
hugo tesson <tesson.hugo@orange.fr>
2025-01-29 13:52:08 +0100

    [ADD] routes and fix in the db

--- back/server/routes/__init__.py
+++ back/server/routes/__init__.py
@@ -2,7 +2,6 @@ import server.routes.login
 import server.routes.lore
 import server.routes.character
 import server.routes.character_utils
-import server.routes.user_requests
 import server.routes.session
 import server.routes.friends
 import server.routes.session_invitation
